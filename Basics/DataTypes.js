5cac3aea5d87a9d490537e9911f9ae95333cd6d3
AteeqAslam769 <110031067+AteeqAslam769@users.noreply.github.com>
2024-02-04 14:19:41 +0000

    Types in JS

---
+++ Basics/DataTypes.js
@@ -0,0 +1,15 @@
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+    // 8.1. Array
+    // 8.2. Date
+    // 8.3. Object
+
+console.log(typeof(null));
+
+// W3 Schools Best for Type Learning
