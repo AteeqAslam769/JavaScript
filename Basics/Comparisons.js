07f1c789459c3d5dc8c2d038b3c39c8594c36ae5
Ateeq Aslam <ateeqaslam769@gmail.com>
2024-02-05 09:28:23 +0500

    Comparison and DataType Revised

---
+++ Basics/Comparisons.js
@@ -0,0 +1,18 @@
+console.log(undefined>=0);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+
+// "<" and ">" Convert null to 0
+// == convert string to number
+
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(null==0);
+
+console.log(null >= "0");
+
+
+// We should not compare values like this
