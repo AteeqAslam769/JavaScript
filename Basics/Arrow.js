5027cca7a1b7386e9fe0e25bb5f8a262b054ec41
AteeqAslam769 <ateeqaslam769@gmail.com>
2024-02-07 15:07:54 +0500

    this done

---
+++ Basics/Arrow.js
@@ -0,0 +1,24 @@
+// Window is the most outer object of browser
+// this //
+// Basic Arrow Function//  const wow = () => {}
+//Implicit return  const wow = (x,y) => x+y
+
+const myObj = {
+    name: "Ateeq",
+    salary:"Alhamdullilah",
+    wow: ()=>console.log((this)),
+}
+
+
+const myObj1 = {
+    name: "Ateeq",
+    salary:"Alhamdullilah",
+    wow: function (){console.log(this)},
+}
+myObj1.wow()
+myObj.wow()
+
+const check = (x,y) =>(x+y)
+p=check(3,6)
+console.log(p);
+
