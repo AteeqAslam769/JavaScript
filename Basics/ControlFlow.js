66752ed4df66b628da244529f22482c4f1f6ec33
AteeqAslam769 <ateeqaslam769@gmail.com>
2024-02-09 20:00:57 +0500

    High order loops

---
+++ Basics/ControlFlow.js
@@ -0,0 +1,7 @@
+// Before Control Flow we learn how code execute an call stack work
+
+// If Else
+// Switch
+// ??       Nullish
+// ?        Ternary 
+// Truthy and Falsy
---
+++ Basics/Loops.js
@@ -0,0 +1,45 @@
+let myArray = [1,2,3,4,5];
+let myObj = {name:"ateeq",
+class:"qt"};
+// For Loops Basic
+// for (let index = 0; index < myArray.length; index++) {
+//     //console.log(myArray[index]);
+// }
+// for (let index = 0; index < Object.keys(myObj).length; index++) {
+//     console.log(Object.entries(myObj));
+// }
+
+// While / Do While
+
+// while (myArray) {
+//     console.log(myArray);
+// }
+
+// do {
+//     console.log("Hello")
+// } while (true);
+
+// For of / For in
+
+// for (const item of myArray) {
+//     console.log(item);
+// }
+// for (const item of myObj) {
+//     console.log(item);
+// }                                    Objects are not ittratable by forof
+
+// for (const key in object) {
+//     console.log(key);
+// }
+
+
+// for (const key in myObj) {
+//     console.log(key,myObj[key]);
+// }
+
+// for (const key in myArray) {
+//    console.log(key);            Work on keys of itterable
+// }
+// Map          Unique value and order sensitive
+
+//foreach  take callback but dont return anyting
